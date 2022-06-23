a2c09fd7088a3ea46ebe93b2bdfccd333e44f689
TabOg <tabitha.ogilvie.2019@live.rhul.ac.uk>
2022-06-23 10:12:42 -0700

    extra tests for not operator

--- misc/psi/lookup/lookup.cpp
+++ misc/psi/lookup/lookup.cpp
@@ -20,8 +20,8 @@
 #include <psiio.h>
 
 #if (defined(__unix__) || defined(__unix) || defined(unix))
-  #include <sys/time.h>
-  #include <sys/resource.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #endif
 
 // Struct to hold command line arguments
@@ -36,12 +36,13 @@ struct CmdLineOpts
   long offset = 0;
 };
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   // PSI STUFF
   // 1. Read in context and pk file
   // 2. Python scripts to gen data (might use utils)
-  // 3. Read in numbers (all numbers in a single row) and (all numbers in a single column)
+  // 3. Read in numbers (all numbers in a single row) and (all numbers in a
+  // single column)
   // 4. Create the query etc. Similar to TestPartial
 
   CmdLineOpts cmdLineOpts;
@@ -65,9 +66,10 @@ int main(int argc, char *argv[])
       "Flag to signify input is in column format.", nullptr)
     .parse(argc, argv);
   // clang-format on
-
+  std::ofstream cout(cmdLineOpts.outFilePath);
   if (cmdLineOpts.nthreads < 1) {
-    std::cerr << "Number of threads must be a postive integer. Setting n = 1." << std::endl;
+    std::cerr << "Number of threads must be a postive integer. Setting n = 1."
+              << std::endl;
     cmdLineOpts.nthreads = 1;
   }
 
@@ -78,18 +80,19 @@ int main(int argc, char *argv[])
   std::shared_ptr<helib::Context> contextp;
   std::unique_ptr<helib::PubKey> pkp;
   std::tie(contextp, pkp) =
-    loadContextAndKey<helib::PubKey>(cmdLineOpts.pkFilePath);
+      loadContextAndKey<helib::PubKey>(cmdLineOpts.pkFilePath);
   HELIB_NTIMER_STOP(readKey);
 
   HELIB_NTIMER_START(readDatabase);
   // Read in database
   helib::Database<helib::Ctxt> database =
-    readDbFromFile(cmdLineOpts.databaseFilePath, contextp, *pkp);
+      readDbFromFile(cmdLineOpts.databaseFilePath, contextp, *pkp);
   HELIB_NTIMER_STOP(readDatabase);
 
   HELIB_NTIMER_START(readQuery);
   // Read in the query data
-  helib::Matrix<helib::Ctxt> queryData = readQueryFromFile(cmdLineOpts.queryFilePath, *pkp);
+  helib::Matrix<helib::Ctxt> queryData =
+      readQueryFromFile(cmdLineOpts.queryFilePath, *pkp);
   HELIB_NTIMER_STOP(readQuery);
 
   HELIB_NTIMER_START(buildQuery);
@@ -97,37 +100,115 @@ int main(int argc, char *argv[])
   const helib::QueryExpr& b = helib::makeQueryExpr(1);
   const helib::QueryExpr& c = helib::makeQueryExpr(2);
 
-  helib::QueryBuilder qb(a);
+  helib::QueryBuilder qba(a);
+  helib::QueryBuilder qbNota(!a);
+  helib::QueryBuilder qbb(b);
+  helib::QueryBuilder qbc(c);
   helib::QueryBuilder qbAnd(a && b);
   helib::QueryBuilder qbOr(a || b);
-  helib::QueryBuilder qbExpand(a || (b && c));
-
-  helib::Query_t query = qb.build(database.columns());
+  helib::QueryBuilder qbExpand1(a || (b && c));
+  helib::QueryBuilder qbExpand2((a || b) && c);
+  // helib::QueryBuilder qbExpand3((a && b) || (a && b));
+  helib::QueryBuilder qbComplex1(a || (!b && c));
+  // helib::QueryBuilder qbComplex2(((!b && c) || (!a))); //<- ambiguous, not
+  // sure how to resolve
+  helib::QueryBuilder qbComplex3((a && !b));
+  std::cout << "query a:\n";
+  helib::Query_t querya = qba.build(database.columns());
+  std::cout << "query not a:\n";
+  helib::Query_t querynota = qbNota.build(database.columns());
+  std::cout << "query b:\n";
+  helib::Query_t queryb = qbb.build(database.columns());
+  std::cout << "query c:\n";
+  helib::Query_t queryc = qbc.build(database.columns());
+  std::cout << "query a and b:\n";
   helib::Query_t queryAnd = qbAnd.build(database.columns());
+  std::cout << "query a or b:\n";
   helib::Query_t queryOr = qbOr.build(database.columns());
-  helib::Query_t queryExpand = qbExpand.build(database.columns());
+  std::cout << "query a or (b and c):\n";
+  helib::Query_t queryExpand1 = qbExpand1.build(database.columns());
+  std::cout << "query (a or b) and c:\n";
+  helib::Query_t queryExpand2 = qbExpand2.build(database.columns());
+  std::cout << "query a or (!b and c):\n";
+
+  helib::Query_t queryComplex1 = qbComplex1.build(database.columns());
+  std::cout << "query a and !b:\n";
+  helib::Query_t queryComplex3 = qbComplex3.build(database.columns());
   HELIB_NTIMER_STOP(buildQuery);
 
-  HELIB_NTIMER_START(lookupSame);
-  auto clean = [](auto& x){x.cleanUp();};
-  auto match = database.contains(query, queryData).apply(clean);
-  HELIB_NTIMER_STOP(lookupSame);
+  HELIB_NTIMER_START(lookupSamea);
+  auto clean = [](auto& x) { x.cleanUp(); };
+  auto matcha = database.contains(querya, queryData).apply(clean);
+  HELIB_NTIMER_STOP(lookupSamea);
+
+  HELIB_NTIMER_START(lookupNot);
+  auto matchnota = database.contains(querynota, queryData).apply(clean);
+  HELIB_NTIMER_STOP(lookupNot);
+
+  HELIB_NTIMER_START(lookupSameb);
+  auto matchb = database.contains(queryb, queryData).apply(clean);
+  HELIB_NTIMER_STOP(lookupSameb);
+
+  HELIB_NTIMER_START(lookupSamec);
+  auto matchc = database.contains(queryc, queryData).apply(clean);
+  HELIB_NTIMER_STOP(lookupSamec);
+
   HELIB_NTIMER_START(lookupAnd);
   auto matchAnd = database.contains(queryAnd, queryData).apply(clean);
   HELIB_NTIMER_STOP(lookupAnd);
+
   HELIB_NTIMER_START(lookupOr);
   auto matchOr = database.contains(queryOr, queryData).apply(clean);
   HELIB_NTIMER_STOP(lookupOr);
-  HELIB_NTIMER_START(lookupExpand);
-  auto matchExpand = database.contains(queryExpand, queryData).apply(clean);
-  HELIB_NTIMER_STOP(lookupExpand);
+
+  HELIB_NTIMER_START(lookupExpand1);
+  auto matchExpand1 = database.contains(queryExpand1, queryData).apply(clean);
+  HELIB_NTIMER_STOP(lookupExpand1);
+
+  HELIB_NTIMER_START(lookupExpand2);
+  auto matchExpand2 = database.contains(queryExpand2, queryData).apply(clean);
+  HELIB_NTIMER_STOP(lookupExpand2);
+
+  HELIB_NTIMER_START(lookupComplex1);
+  auto matchcomplex1 = database.contains(queryComplex1, queryData).apply(clean);
+  HELIB_NTIMER_STOP(lookupComplex1);
+
+  HELIB_NTIMER_START(lookupComplex3);
+  auto matchcomplex3 = database.contains(queryComplex3, queryData).apply(clean);
+  HELIB_NTIMER_STOP(lookupComplex3);
 
   HELIB_NTIMER_START(writeResults);
   // Write results to file
-  writeResultsToFile(cmdLineOpts.outFilePath, match, cmdLineOpts.offset);
-  writeResultsToFile(cmdLineOpts.outFilePath+"_and", matchAnd, cmdLineOpts.offset);
-  writeResultsToFile(cmdLineOpts.outFilePath+"_or", matchOr, cmdLineOpts.offset);
-  writeResultsToFile(cmdLineOpts.outFilePath+"_expand", matchExpand, cmdLineOpts.offset);
+  writeResultsToFile(cmdLineOpts.outFilePath + "_a",
+                     matcha,
+                     cmdLineOpts.offset);
+  writeResultsToFile(cmdLineOpts.outFilePath + "_!a",
+                     matchnota,
+                     cmdLineOpts.offset);
+  writeResultsToFile(cmdLineOpts.outFilePath + "_b",
+                     matchb,
+                     cmdLineOpts.offset);
+  writeResultsToFile(cmdLineOpts.outFilePath + "_c",
+                     matchc,
+                     cmdLineOpts.offset);
+  writeResultsToFile(cmdLineOpts.outFilePath + "_and",
+                     matchAnd,
+                     cmdLineOpts.offset);
+  writeResultsToFile(cmdLineOpts.outFilePath + "_or",
+                     matchOr,
+                     cmdLineOpts.offset);
+  writeResultsToFile(cmdLineOpts.outFilePath + "_expand1",
+                     matchExpand1,
+                     cmdLineOpts.offset);
+  writeResultsToFile(cmdLineOpts.outFilePath + "_expand2",
+                     matchExpand2,
+                     cmdLineOpts.offset);
+  writeResultsToFile(cmdLineOpts.outFilePath + "_aOr_!bAndc",
+                     matchcomplex1,
+                     cmdLineOpts.offset);
+  writeResultsToFile(cmdLineOpts.outFilePath + "_aAnd!b",
+                     matchcomplex3,
+                     cmdLineOpts.offset);
   HELIB_NTIMER_STOP(writeResults);
 
   std::ofstream timers("times.log");
@@ -139,16 +220,16 @@ int main(int argc, char *argv[])
   std::ofstream usage("usage.log");
   if (usage.is_open()) {
     struct rusage rusage;
-    getrusage( RUSAGE_SELF, &rusage );
-    usage << "\n  rusage.ru_utime="<<rusage.ru_utime.tv_sec << '\n';
-    usage << "  rusage.ru_stime="<<rusage.ru_utime.tv_sec << '\n';
-    usage << "  rusage.ru_maxrss="<<rusage.ru_maxrss << '\n';
-    usage << "  rusage.ru_minflt="<<rusage.ru_minflt << '\n';
-    usage << "  rusage.ru_majflt="<<rusage.ru_majflt << '\n';
-    usage << "  rusage.ru_inblock="<<rusage.ru_inblock << '\n';
-    usage << "  rusage.ru_oublock="<<rusage.ru_majflt << '\n';
-    usage << "  rusage.ru_nvcsw="<<rusage.ru_nvcsw << '\n';
-    usage << "  rusage.ru_nivcsw="<<rusage.ru_minflt << std::endl;
+    getrusage(RUSAGE_SELF, &rusage);
+    usage << "\n  rusage.ru_utime=" << rusage.ru_utime.tv_sec << '\n';
+    usage << "  rusage.ru_stime=" << rusage.ru_utime.tv_sec << '\n';
+    usage << "  rusage.ru_maxrss=" << rusage.ru_maxrss << '\n';
+    usage << "  rusage.ru_minflt=" << rusage.ru_minflt << '\n';
+    usage << "  rusage.ru_majflt=" << rusage.ru_majflt << '\n';
+    usage << "  rusage.ru_inblock=" << rusage.ru_inblock << '\n';
+    usage << "  rusage.ru_oublock=" << rusage.ru_majflt << '\n';
+    usage << "  rusage.ru_nvcsw=" << rusage.ru_nvcsw << '\n';
+    usage << "  rusage.ru_nivcsw=" << rusage.ru_minflt << std::endl;
   }
 #endif
 
