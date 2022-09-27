51feb9300f6eb058b50270cf133a682cd4b6fc68
TabOg <61025163+TabOg@users.noreply.github.com>
2022-09-27 16:29:53 +0100

    Update include/helib/query.h
    Co-authored-by: Jack Crawford <jack.crawford@sky.com>

--- include/helib/query.h
+++ include/helib/query.h
@@ -546,7 +546,7 @@ private:
   /**
    * @brief Take a string in Reverse Polish Notation and produce a
    * vector of vectors representing a logically equivalent `AND` of `OR`s.
-   * @param s string in RPN, operators given by &&, || or !
+   * @param s String in RPN, operators given by `&&`, `||` or `!`
    * @return vecvec a logically equivalent conjunction of disjunctions of
    * either columns or their negations. (i + 1) corresponds to column i, and
    * negatives correspond to negations of columns.
